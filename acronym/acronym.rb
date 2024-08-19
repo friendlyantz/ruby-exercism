7e35a146007d7e716d2b95bda50efaf0d92e826d
anton <friendlyantz@pm.me>
2024-08-20 05:40:37 +1000

    Acronym solution

--- acronym/acronym.rb
+++ acronym/acronym.rb
@@ -1,9 +1,30 @@
-class Acronym
-  def self.abbreviate(input)
-    input
-      .split(/\W/)
+# frozen_string_literal: true
+
+module Acronym
+  def self.abbreviate(phrase)
+    raise StandardError, "must be a string. we got: #{phrase}" unless phrase.is_a?(String)
+
+    phrase
+      .split(/[\s-]+/)
       .map(&:chr)
       .join
       .upcase
+  end
+end
+
+class Abbreviator
+  include Acronym
+
+  attr_reader :phrase, :abbreviation
+
+  def initialize(phrase)
+    @phrase = phrase
+    @abbreviation = abbreviate
+  end
+
+  private
+
+  def abbreviate
+    Acronym.abbreviate(phrase)
   end
 end
