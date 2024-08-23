df78ef060290d7c106259be6e52323620345edc7
anton <friendlyantz@pm.me>
2024-08-24 00:44:05 +1000

    use dynamic method generation

--- space-age/space_age.rb
+++ space-age/space_age.rb
@@ -1,26 +1,42 @@
-require 'bigdecimal'
+class UnknownPlanet < StandardError
+  def initialize(message = 'we are not aware of this planet')
+    super
+  end
+end
 
 class SpaceAge
-  ORBITAL_PERIODS = {
-    mercury: 0.2408467,
-    venus: 0.61519726,
-    earth: 1.0,
-    mars: 1.8808158,
-    jupiter: 11.862615,
-    saturn: 29.447498,
-    uranus: 84.016846,
-    neptune: 164.79132,
-  }
+  seconds_on_earth = 31_557_600
 
-  SECONDS_ON_EARTH = 31557600
+  ORBITAL_PERIODS = {
+    mercury: Rational(0.2408467, 1),
+    venus: Rational(0.61519726, 1),
+    earth: 1r,
+    mars: Rational(1.8808158, 1),
+    jupiter: Rational(11.862615, 1),
+    saturn: Rational(29.447498, 1),
+    uranus: Rational(84.016846, 1),
+    neptune: Rational(164.79132, 1)
+  }.transform_values { _1 * seconds_on_earth }
 
   def initialize(seconds)
-    @seconds = BigDecimal(seconds)
+    @seconds = seconds
   end
 
-  ORBITAL_PERIODS.each do |name, period|
-    define_method :"on_#{name}" do
-      (@seconds / (ORBITAL_PERIODS[name] * SECONDS_ON_EARTH ))
+  def method_missing(method_name, *args, &block)
+    if method_name.to_s.start_with?('on_')
+      planet = method_name.to_s.delete_prefix('on_').to_sym
+      raise UnknownPlanet unless ORBITAL_PERIODS.key?(planet)
+
+      self.class.define_method(method_name) do
+        @seconds / ORBITAL_PERIODS[planet]
+      end
+      send(method_name)
+    else
+      super
     end
+  end
+
+  def respond_to_missing?(method_name, include_private = false)
+    method_name.to_s.start_with?('on_') || super
   end
 end
